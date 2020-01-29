9845b947a0fa004f4f580019bc28aa81f8beaf4d
Erik <erik.magnus.olsson.sundh@gmail.com>
2020-01-29 09:56:30 +0100

    Bootstrapped backend

---
+++ forening-backend/src/main/java/se/foreningsdialog/forening/ForeningApplication.java
@@ -0,0 +1,13 @@
+package se.foreningsdialog.forening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeningApplication.class, args);
+    }
+
+}
---
+++ forening-backend/src/test/java/se/foreningsdialog/forening/ForeningApplicationTests.java
@@ -0,0 +1,13 @@
+package se.foreningsdialog.forening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
