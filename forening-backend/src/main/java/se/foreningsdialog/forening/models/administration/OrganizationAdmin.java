b6ba0f39ea106bdd5a603ac1ed8147c408e48a26
Oleg Meditskiy <olegmeditsky@gmail.com>
2020-02-03 16:11:35 +0100

    Edited Administration classes

--- forening-backend/src/main/java/se/foreningsdialog/forening/models/administration/OrganizationAdmin.java
+++ forening-backend/src/main/java/se/foreningsdialog/forening/models/administration/OrganizationAdmin.java
@@ -1,10 +1,22 @@
 package se.foreningsdialog.forening.models.administration;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import se.foreningsdialog.forening.models.administration.constants.AdminRoles;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@DiscriminatorValue("ORGANIZATION_ADMIN")
 public class OrganizationAdmin extends AbstractAdministration {
     private boolean canEditDocuments;
     @Enumerated(EnumType.ORDINAL)
--- forening-backend/src/main/java/se/foreningsdialog/forening/models/administration/OrganizationSuperAdmin.java
+++ forening-backend/src/main/java/se/foreningsdialog/forening/models/administration/OrganizationSuperAdmin.java
@@ -1,10 +1,22 @@
 package se.foreningsdialog.forening.models.administration;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import se.foreningsdialog.forening.models.Organization;
 import se.foreningsdialog.forening.models.administration.constants.SuperAdminRoles;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@DiscriminatorValue("ORGANIZATION_SUPER_ADMIN")
 public class OrganizationSuperAdmin extends AbstractAdministration {
 
     private SuperAdminRoles role = SuperAdminRoles.ORGANIZATION_SUPER_ADMIN;
