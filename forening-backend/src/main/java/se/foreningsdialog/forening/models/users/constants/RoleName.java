5eeb402d89f42647f5859c96fe36c3a30e3ab68a
Oleg Meditskiy <olegmeditsky@gmail.com>
2020-02-16 15:37:17 +0100

    added backend for login

---
+++ forening-backend/src/main/java/se/foreningsdialog/forening/models/users/constants/RoleName.java
@@ -0,0 +1,9 @@
+package se.foreningsdialog.forening.models.users.constants;
+
+public enum RoleName {
+    ROLE_MEMBER,
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_SUPER_ADMIN,
+    ROLE_MAIN_ADMIN
+}
