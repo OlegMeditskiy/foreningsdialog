b7084937d36d3af5b9f6e3e69eb763381558d109
Oleg Meditskiy <olegmeditsky@gmail.com>
2020-03-03 16:23:31 +0100

    Added method to AssociationService to response associations by user

--- forening-backend/src/main/java/se/foreningsdialog/forening/controllers/AssociationController.java
+++ forening-backend/src/main/java/se/foreningsdialog/forening/controllers/AssociationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import se.foreningsdialog.forening.models.Association;
 import se.foreningsdialog.forening.payload.AssociationResponse;
 import se.foreningsdialog.forening.payload.PagedResponse;
 import se.foreningsdialog.forening.repository.AssociationRepository;
@@ -28,7 +27,7 @@ public class AssociationController {
     private PagedResponse<AssociationResponse> getAssociations(@CurrentUser UserPrincipal currentUser,
                                                               @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
                                                               @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size){
-            return associationService.getAllPolls(currentUser,page,size);
+            return associationService.getAllAssociations(currentUser,page,size);
     }
 
 
